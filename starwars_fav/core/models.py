28502c87512f75cc2cbf27e55808be87064cd03b
abhishekram <abhishek8816@gmail.com>
2019-08-04 08:26:10 +0530

    add the models for saving favorite information

--- starwars_fav/core/models.py
+++ starwars_fav/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class PlanetFavorite(models.Model):
+    """Model for saving Planet information to the local Database."""
+    external_id = models.IntegerField()
+    name = models.CharField(max_length=500)
+
+
+class MovieFavorite(models.Model):
+    """Model for saving the Movie information to the local Database."""
+    external_id = models.IntegerField()
+    title = models.CharField(max_length=500)
